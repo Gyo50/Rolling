947c2361c63b2b425af027b01d99aaee0e0bd30f
Oh Young gyo <oey157@nate.com>
2025-11-06 14:36:24 +0900

    tailwind 적용완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./components/**/*{js.jsx,.tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
        colors: {
