a14e87991a9b1ef8c12415b68b367f2be6c79fea
홍요한 <ghddygks45@gmail.com>
2025-11-07 14:48:19 +0900

    feat: Text Field ui 작성(input, selectbox)

--- src/Text_Field/Text_field.jsx
+++ src/Text_Field/Text_field.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Input from "./Input";
+import SelectBox from "./SelectBox";
 
 function Text_field() {
   return (
-    <></>
-  )
+    <>
+      <Input />
+      <SelectBox />
+    </>
+  );
 }
 
-export default Text_field
+export default Text_field;
