1979a6cc71b763465beb1da7ce6bea1b6899d964
duddj122352-design <duddj122352@gmail.com>
2025-11-08 00:15:16 +0900

    feat CSS수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Option from './Option/Option.jsx';
-
+import Header from './Header/Header.jsx'
+import MessageHeader from './Header/MessageHeader.jsx'
+import MobileHeader from './Header/MobileHeader.jsx'
 
 
 function App() {
   return (
     <>
       <Option/>
+      <Header/>
+      <MessageHeader/>
+      <MobileHeader/>
     </>
 
   );
