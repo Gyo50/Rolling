dd7dd0c05c1e800ec7b3f9e922aace58381e60c2
Oh Young gyo <oey157@nate.com>
2025-11-09 15:48:14 +0900

    적용법

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import Emoji from "./Option/Emoji";
 
 function App() {
-  const [open, setOpen] = useState(false);
 
   return (
     <>
-      <button onClick={() => setOpen(!open)}>
-        {open ? "이모지 닫기" : "이모지 열기"}
-      </button>
-
-      {open && <Emoji />}
     </>
   );
 }
