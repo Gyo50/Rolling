e281afb5996738ee1406702612be73e0fe0646b1
Oh Young gyo <oey157@nate.com>
2025-11-11 01:34:23 +0900

    messagelayout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
-import MessageHeader from "./Header/MessageHeader"
-import Header from "./Header/Header"
-import MobileHeader from "./Header/MobileHeader";
-
+import Messagepage from "./MessagePage/MessagePage" 
 
 function App() {
   return (
     <>
-    <Header/>
-    <MessageHeader/>
-    <MobileHeader/>
+      <Messagepage/>
     </>
   );
 }
