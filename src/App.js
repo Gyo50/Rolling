cb3a1e88b6ac4a20c2fce9b54936fa217f344747
Oh Young gyo <oey157@nate.com>
2025-11-15 04:26:26 +0900

    recipient 재 커밋

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import CreatePostPage from './CreatePostPage/CreatePostPage'
 import OwnerPage from './RollingPage/OwnerPage'
 import RecentPage from './ListPage/RecentPage'
 import Messagepage from './MessagePage/MessagePage'
-// import RecipientPage from './RollingPage/RecipientPage'
+import RecipientPage from './RollingPage/RecipientPage'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
       <Route path="/list" element={<ListPage />} />
       <Route path="/recent" element={<RecentPage />} />
       <Route path="/post" element={<CreatePostPage />} />
-      <Route path="/post/:id" element={<OwnerPage />} />
+      <Route path="/post/:id" element={<Messagepage />} />
+      <Route path="/post/:id/message" element={<Messagepage />} />
+      <Route path="/post/:id" element={<RecipientPage />} />
+      <Route path="/post/:id/owner" element={<OwnerPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
